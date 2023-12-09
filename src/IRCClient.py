f474963049b2a0ac7fa5e008c4a33e7aa38c64a7
n1gtm <40951780+gx56q@users.noreply.github.com>
2023-12-10 01:25:01 +0500

    finished main functionality

--- src/IRCClient.py
+++ src/IRCClient.py
@@ -1,25 +1,27 @@
 import socket
 import threading
+import time
+from select import select
 
 
 class IRCClient:
-    def __init__(self, nickname, server, port=6667):
+    def __init__(self, username, server, port=6667, callback=None):
+        self.message_callback = callback
         self.listen_thread = None
         self.server = server
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.password = None
-        self.nickname = nickname
-        self.active_channel = None
-        self.channels = []
-        self.users = []
+        self.username = username
 
     def connect(self):
         self.socket.connect((self.server, self.port))
 
     def auth(self):
-        self.send_data('NICK', self.nickname)
-        self.send_data('USER', f"{self.nickname} 0 * :{self.nickname}")
+        time.sleep(0.5)
+        self.send_data('NICK', self.username)
+        time.sleep(0.5)
+        self.send_data('USER', f"{self.username} 0 * :{self.username}")
 
     def disconnect(self, message=""):
         self.send_data('QUIT', f":{message}")
@@ -27,40 +29,57 @@ class IRCClient:
 
     def send_data(self, command, message=''):
         if message:
-            full_message = f"{command} {message}\n"
+            full_message = f"{command} {message}\r\n"
         else:
             full_message = f"{command}\n"
         self.socket.send(full_message.encode('utf-8'))
 
     def get_response(self):
-        return self.socket.recv(2048).decode('utf-8')
+        try:
+            return self.socket.recv(1024).decode('utf-8', errors='ignore')
+        except UnicodeDecodeError:
+            return ''
 
     def join_channel(self, channel_name):
-        self.users = []
         self.send_data('JOIN', channel_name)
-        self.active_channel = channel_name
-        if channel_name not in self.channels:
-            self.channels.append(channel_name)
 
-    def send_message(self, message):
-        if self.active_channel:
-            self.send_data('PRIVMSG', f"{self.active_channel} :{message}")
-        else:
-            raise ValueError('No active channel')
+    def send_message(self, channel, message):
+        max_message_length = 250
+        message_chunks = [message[i:i + max_message_length] for i in
+                          range(0, len(message), max_message_length)]
+
+        for chunk in message_chunks:
+            self.send_data('PRIVMSG', f"{channel} :{chunk}")
+            time.sleep(0.5)
+
+    def change_nick(self, new_nick):
+        self.send_data('NICK', new_nick)
+        self.username = new_nick
+
+    def leave_channel(self, channel_name):
+        self.send_data('PART', channel_name)
 
     def listen(self):
         while True:
-            message = self.get_response()
-            if message.startswith('PING'):
-                self.send_data('PONG', message.split()[1])
-
-            elif " 353 " in message:
-                parts = message.split()
-                # self.users += message
-            print(message)
+            (readable, writable, errored) = select([self.socket], [],
+                                                   [self.socket], 0.1)
+            if readable:
+                read_buffer = ""
+                read_buffer += self.get_response()
+                temp = str.split(read_buffer, "\n")
+                temp.pop()
+                for line in temp:
+                    line = str.rstrip(line)
+                    line = str.split(line)
+                    if len(line) >= 1:
+                        if line[0] == 'PING':
+                            self.send_data('PONG', line[1])
+                        else:
+                            self.message_callback(line)
 
     def start_listening(self):
         self.listen_thread = threading.Thread(target=self.listen)
+        self.listen_thread.daemon = True
         self.listen_thread.start()
 
     def get_channels(self):
--- src/main.py
+++ src/main.py
@@ -1,28 +1,11 @@
-import time
-import IRCClient
+from tkinter import Tk
+import Ui
 
 
 def main():
-    server = 'chat.freenode.net'
-    port = 6667
-    nickname = 'web-43'
-    client = IRCClient.IRCClient(nickname, server, port)
-    print('Connecting to server...')
-    client.connect()
-    print('Starting listening thread...')
-    client.start_listening()
-    print('Authenticating...')
-    client.auth()
-    time.sleep(5)
-    print('Joining channel...')
-    client.join_channel('#freenode')
-    time.sleep(5)
-    print('Users in channel:')
-    print(client.users)
-    print('Getting channels...')
-    client.get_channels()
-    # print('Sending message...')
-    # client.send_message('Hello, world!')
+    root = Tk()
+    app = Ui.Window(root)
+    root.mainloop()
 
 
 if __name__ == '__main__':
